2a818956ce6e35074b11d7be85f6b26831d66346
Abdullah Enes Can <aenesc1@gmail.com>
2022-02-20 16:56:10 +0300

    Register component added

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -22,6 +22,7 @@ export default class UserStore {
       store.commonStore.setToken(user.token);
       runInAction(() => (this.user = user));
       history.push("/activities");
+      store.modalStore.closeModal();
     } catch (error) {
       throw error;
     }
@@ -42,4 +43,17 @@ export default class UserStore {
       console.log(error);
     }
   };
+
+  register = async (creds: UserFormValues) => {
+    try {
+      const user = await agent.Account.register(creds);
+      store.commonStore.setToken(user.token);
+      runInAction(() => (this.user = user));
+      history.push("/activities");
+      store.modalStore.closeModal();
+    } catch (error) {
+      throw error;
+    }
+  };
+
 }
